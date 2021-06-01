70e127ba7b96cfbf1dc683a1cf179ef7fca9cd83
dariwn <perezalboresd@gmail.com>
2021-06-01 16:48:41 -0500

    Update crear.blade.php

--- resources/views/curriculo/crear.blade.php
+++ resources/views/curriculo/crear.blade.php
@@ -294,7 +294,7 @@ var element2 = -1;
                 <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
                 <div class="form-group">
                 <label for="nombre">Escuela de Procedencia</label>
-                <input type="text" name="escuela" required value="Tecnologico Nacional De México Campus Tuxtla Gutierrez" class="form-control" placeholder="Escuela">
+                <input type="text" name="escuela" required value="Tecnológico Nacional De México Campus Tuxtla Gutiérrez" class="form-control" placeholder="Escuela">
                 </div>
                 </div>
                 <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
